8ee55e1be9acec6bdde55c30311781fb53fd2a18
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-12-14 20:04:19 -0800

    replace loginuser from api with mutation

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,12 +1,14 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/react-hooks';
 
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
