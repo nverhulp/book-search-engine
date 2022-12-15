442dbed450e2e729fa617045a4f423653187c2ea
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-12-14 19:26:20 -0800

    removeBook mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -43,3 +43,20 @@ export const SAVE_BOOK = gql `
     }
 `;
 
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username 
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
