6c651eae46ca1aec0aeb42be039fa8916c5dbf17
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-12-14 19:25:08 -0800

    saveBook mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,3 +25,21 @@ export const ADD_USER = gql`
     }
 `;
 
+export const SAVE_BOOK = gql `
+    mutation saveBook($input: SavedBookInput) {
+        saveBook(input: $input) {
+            username
+            _id
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
