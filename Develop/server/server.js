fad2bf219c27cee6c16a27383a8ec46f28b052fd
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-12-17 01:00:52 -0800

    started over

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,9 +14,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
